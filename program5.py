5a66c46554093b7fb25eda8dc6de3c8ae98d96ec
Basetty Lakshmi Sagar <lakshmisagarbasetty27@gmail.com>
2026-07-24 20:26:40 +0530

    Delete program5.py

--- program5.py
+++
@@ -1,9 +0,0 @@
-import numpy
-import pandas
-import matplotlib
-import sklearn
-
-print("NumPy Version      :", numpy.__version__)
-print("Pandas Version     :", pandas.__version__)
-print("Matplotlib Version :", matplotlib.__version__)
-print("Scikit-learn Version:", sklearn.__version__)
